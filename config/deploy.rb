6e74ce8c76773c85c18639df0c0ad13f8aa2ad8c
vmoor <vladimir.moor@gmx.de>
2012-12-06 22:47:08 +0100

    deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,15 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+  namespace :assets do
+  	desc "Precompile assets on local machine and upload them to the server."
+  	task :precompile, roles: :web, except: {no_release: true} do
+  		run locally "rake assets:precompile"
+  		find_servers_for_task(current_task).each do |server|
+  			run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
+  		end
+  	end
+  end
 end
 
 after "deploy:update_code", "bundle:install"
