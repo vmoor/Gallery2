9f3ba41aa5a0b4e4f897dbd5d1ef54ca31012163
vmoor <vladimir.moor@gmx.de>
2012-12-06 23:03:33 +0100

    fix style

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,7 @@ namespace :deploy do
   namespace :assets do
   	desc "Precompile assets on local machine and upload them to the server."
   	task :precompile, roles: :web, except: {no_release: true} do
-  		run locally "rake assets:precompile"
+  		run_locally "rake assets:precompile"
   		find_servers_for_task(current_task).each do |server|
   			run_locally "rsync -vr --exclude='.DS_Store' public/assets #{user}@#{server.host}:#{shared_path}/"
   		end
