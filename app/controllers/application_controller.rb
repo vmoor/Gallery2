bd33d03c3dded4a2246d3019bc14db237e57f3b4
vmoor <vladimir.moor@gmx.de>
2012-12-15 19:34:08 +0100

    redirect user to login page after logging out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
 	 user_path(current_user)
-	end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+     new_user_session_path
+  end
 end
